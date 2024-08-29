c75afe5752ed3e63bae8eac91b207c3967f35a8b
elkacem <belkacemyesse@gmail.com>
2024-08-29 16:00:54 +0100

    remove comment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,29 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//        // Get all subcategory IDs
-//        $subcategoryIds = Subcategory::all()->pluck('id')->toArray();
-//        // User::factory(10)->create();
-//
-////        User::factory()->create([
-////            'name' => 'Test User',
-////            'email' => 'test@example.com',
-////        ]);
-//        User::factory()->create([
-//            'name' => 'belo',
-//            'email' => 'belo@mail.com',
-//        ]);
-//        Notification::factory(3)->create();
-//        Business::factory()->count(50)->create([
-//            'subcategory_id' => function() use ($subcategoryIds) {
-//                return $this->faker->randomElement($subcategoryIds);
-//            },
-//
-//        $this->call([
-//            CategorySeeder::class,
-//            SubcategorySeeder::class,
-//            // Add more seeders here if needed
-//        ]);
         User::factory()->create([
             'name' => 'belo',
             'email' => 'belkacemyesse@gmail.com', // Updated email
